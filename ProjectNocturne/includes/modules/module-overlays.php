8e37d980b5656e646b0ff8dd9edf092086f270e6
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-07-05 14:17:19 -0500

    Add files via upload

---
+++ ProjectNocturne/includes/modules/module-overlays.php
@@ -0,0 +1,9 @@
+<div class="module-overlay disabled">
+    <?php include 'includes/menus/menu-paletteColors.php'; ?>
+    <?php include 'includes/menus/menu-alarm.php'; ?>
+    <?php include 'includes/menus/menu-timer.php'; ?>
+    <?php include 'includes/menus/menu-worldClock.php'; ?>
+    <?php include 'includes/menus/menu-sounds.php'; ?>
+    <?php include 'includes/menus/menu-country.php'; ?>
+    <?php include 'includes/menus/menu-timezone.php'; ?>
+</div>
